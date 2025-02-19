7a03b15749e6058c4b710a332947a827b793feba
KulsumShaikh16 <160030040+KulsumShaikh16@users.noreply.github.com>
2025-02-20 03:42:12 +0500

    Update growth.py

--- growth.py
+++ growth.py
@@ -75,16 +75,15 @@ if uploaded_file:
                 file_name = file.name.replace(file_ext, ".csv")
                 mime_type = "text/csv"
 
-            elif convert_format == "xlsx":
-                df.to_excel(buffer, index=False)
-                file_name = file.name.replace(file_ext, ".xlsx")
-                mime_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                buffer.seek(0)
-                st.download_button(
-                    label=f"Click here to download {file_name}",
-                    data=buffer,
-                    file_name=file_name,
-                    mime=mime_type,
-                )
-
+           if convert_format == "xlsx":
+    df.to_excel(buffer, index=False)
+    file_name = file.name.replace(file_ext, ".xlsx")
+    mime_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    buffer.seek(0)
+    st.download_button(
+        label=f"Click here to download {file_name}",
+        data=buffer,
+        file_name=file_name,
+        mime=mime_type,
+    )
         st.success("All files processed successfully.")
